2facac1f88a8570254e6553516fdd939a490f10e
Aboudoc <aboutika.reda@gmail.com>
2022-11-22 18:07:50 +0100

    update

--- pages/index.jsx
+++ pages/index.jsx
@@ -55,28 +55,35 @@ const Home = () => {
   };
 
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <div>
+    <div className="flex flex-col items-center justify-center py-8 gap-y-3">
+      <div className="flex flex-col w-full justify-center items-center gap-y-2">
         <input
-          onChange={(e) => setWalletAddress(e.target.value)}
+          disabled={fetchForCollection}
+          className="w-2/5 bg-slate-100 py-2 px-2 rounded-lg test-gray-800 focus:outline-blue-300 disabled:bg-slate-50 disabled:text-gray-50"
+          onChange={(e) => {
+            setWalletAddress(e.target.value);
+          }}
           value={wallet}
           type={"text"}
           placeholder="Add your wallet address"
         ></input>
         <input
+          className="w-2/5 bg-slate-100 py-2 px-2 rounded-lg test-gray-800 focus:outline-blue-300 disabled:bg-slate-50 disabled:text-gray-50"
           onChange={(e) => setCollectionAddress(e.target.value)}
           value={collection}
           type={"text"}
           placeholder="Add the collection address"
         ></input>
-        <label>
+        <label className="text-gray-600">
           <input
             onChange={(e) => setFetchForCollection(e.target.checked)}
             type={"checkbox"}
+            className="mr-2"
           ></input>
           Fetch for collection
         </label>
         <button
+          className="disabled:bg-slate-500 text-white bg-blue-400 px-4 py-2 mt-3 rounded-sm w-1/5"
           onClick={() => {
             if (fetchForCollection) {
               fetchNFTsForCollection();
@@ -88,7 +95,7 @@ const Home = () => {
           Let's go!
         </button>
       </div>
-      <div>
+      <div className="flex flex-wrap gap-y-12 mt-4 w-5/6 gap-x-2 justify-center">
         {NFTs.length &&
           NFTs.map((nft) => {
             return <NFTCard nft={nft}></NFTCard>;
