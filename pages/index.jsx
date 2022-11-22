3906db95512243b951f3c687b06f4b26c8081f05
Aboudoc <aboutika.reda@gmail.com>
2022-11-22 09:30:31 +0100

    updated

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,6 +5,30 @@ import { useState } from "react";
 const Home = () => {
   const [wallet, setWalletAddress] = useState("");
   const [collection, setCollectionAddress] = useState("");
+  const [NFTs, setNFTs] = useState([]);
+
+  const fetchNFTs = async () => {
+    let nfts;
+    console.log("fetching nfts");
+    if (!collection.length) {
+      // Setup request options:
+      var requestOptions = {
+        method: "GET",
+      };
+      const apiKey = "1ies5QbTezWH1atYmleFGuywUWWxYxJz";
+      const baseURL = `https://eth-mainnet.g.alchemy.com/nft/v2/${apiKey}/getNFTs/`;
+      const fetchURL = `${baseURL}?owner=${wallet}`;
+
+      nfts = await fetch(fetchURL, requestOptions).then((data) => data.json());
+    } else {
+    }
+
+    if (nfts) {
+      console.log("nfts: ", nfts);
+      // setNFTs(nfts);
+    }
+  };
+
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <div>
@@ -22,7 +46,7 @@ const Home = () => {
         ></input>
         <label>
           <input type={"checkbox"}></input>
-          <button>Let's go!</button>
+          <button onClick={() => fetchNFTs()}>Let's go!</button>
         </label>
       </div>
     </div>
